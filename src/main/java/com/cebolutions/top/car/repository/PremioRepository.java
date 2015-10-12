6381c6b7534f693cc99d4600469436e3897a038c
Matheusss <matheusb95@gmail.com>
2015-10-12 16:01:09 -0300

    mergin

--- src/main/java/com/cebolutions/top/car/repository/PremioRepository.java
+++ src/main/java/com/cebolutions/top/car/repository/PremioRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import com.cebolutions.top.car.entity.Premio;
 
 public interface PremioRepository extends CrudRepository<Premio, Long> {
-
 }
