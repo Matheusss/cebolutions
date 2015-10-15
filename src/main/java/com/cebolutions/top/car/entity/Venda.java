55716e4a25b5f6198b9b6a8469c5baeed9fef74c
Matheusss <matheusb95@gmail.com>
2015-10-15 15:52:26 -0300

    Retira ENUM da entity vendas

--- src/main/java/com/cebolutions/top/car/entity/Venda.java
+++ src/main/java/com/cebolutions/top/car/entity/Venda.java
@@ -4,13 +4,9 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import com.cebolutions.top.car.enums.FormaPagamento;
 
 @Entity
 @Table(name="VENDA")
@@ -33,10 +29,6 @@ public class Venda extends AbstractEntity {
 	@ManyToOne
 	@JoinColumn(name="CARRO_ID")
 	private Carro carro;
-	
-	@Enumerated(EnumType.STRING)
-	@Column(name="FORMA_PAGAMENTO")
-	private FormaPagamento formaPagamento;
 
 	public void setDataVenda(LocalDateTime dataVenda) {
 		this.dataVenda = dataVenda;
@@ -64,14 +56,6 @@ public class Venda extends AbstractEntity {
 
 	public void setCarro(Carro carro) {
 		this.carro = carro;
-	}
-
-	public FormaPagamento getFormaPagamento() {
-		return formaPagamento;
-	}
-
-	public void setFormaPagamento(FormaPagamento formaPagamento) {
-		this.formaPagamento = formaPagamento;
 	}
 
 }
