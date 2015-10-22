f1bc0b9a5b54fc986326a03648422d2d0ee662b7
Matheusss <matheusb95@gmail.com>
2015-10-22 16:15:09 -0200

    Cria metodo p buscar estoque por carro

--- src/main/java/com/cebolutions/top/car/repository/EstoqueRepository.java
+++ src/main/java/com/cebolutions/top/car/repository/EstoqueRepository.java
@@ -2,8 +2,11 @@ package com.cebolutions.top.car.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.cebolutions.top.car.entity.CarroVenda;
 import com.cebolutions.top.car.entity.Estoque;
 
 public interface EstoqueRepository extends CrudRepository<Estoque, Long> {
 
+	Estoque findByCarroVenda(CarroVenda carro);
+	
 }
