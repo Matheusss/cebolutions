755785a9e1c5ddb058e28bf51829e3fc9fdc3941
yyuri <ygor_cavalera@hotmail.com>
2015-10-24 21:48:03 -0200

    Car
    Ajuste CarController e details.html

--- src/main/webapp/site/src/app/controllers/car/carController.js
+++ src/main/webapp/site/src/app/controllers/car/carController.js
@@ -1,29 +1,27 @@
-angular.module('cebolutions.controllers')
+(function() {
+  'use strict';
+
+    angular.module('cebolutions.controllers')
     .controller('CarController', [
         '$scope', '$location', '$timeout', '$http', 'urlConfig', '$state', 'Feedback', function($scope, $location, $timeout, $http, urlConfig, $state, Feedback) {
 
-        $scope.car = [];
+        
 
-        $scope.mudarCor = function(car){
-            var car = document.getElementById("car");
-            alert="asdas"
-            switch(cor){
-                case 0:
-                    car.src="../../../img/mustang/default.png";
-                break;
-                case 1:
-                    car.src="../../../img/mustang/defaultAzul.png";
-                break;
-                case 2:
-                    car.src="../../../img/mustang/defaultPreto.png";
-                break;
-                case 3:
-                    car.src="../../../img/mustang/defaultAmarelo.png";
-                break;
-                case 4:
-                    car.src="capturar16.png";
-                break;
-            }
-        }
+        $scope.car = {
+            red: ["http://192.168.1.8:80/img/mustang/red/redBack.png","http://192.168.1.8:80/img/mustang/red/redSide.png","http://192.168.1.8:80/img/mustang/red/redFront.png" ],
+            yellow: ["http://192.168.1.8:80/img/mustang/yellow/yellowBack.png","http://192.168.1.8:80/img/mustang/yellow/yellowSide.png","http://192.168.1.8:80/img/mustang/yellow/yellowFront.png" ],
+            gray: ["http://192.168.1.8:80/img/mustang/gray/grayBack.png","http://192.168.1.8:80/img/mustang/gray/graySide.png","http://192.168.1.8:80/img/mustang/gray/grayFront.png" ],
+            white: ["http://192.168.1.8:80/img/mustang/white/whiteBack.png","http://192.168.1.8:80/img/mustang/white/whiteSide.png","http://192.168.1.8:80/img/mustang/white/whiteFront.png" ],
+            black: ["http://192.168.1.8:80/img/mustang/black/blackBack.png","http://192.168.1.8:80/img/mustang/black/blackSide.png","http://192.168.1.8:80/img/mustang/black/blackFront.png" ],
+            blue: ["http://192.168.1.8:80/img/mustang/blue/blueBack.png","http://192.168.1.8:80/img/mustang/blue/blueSide.png","http://192.168.1.8:80/img/mustang/blue/blueFront.png" ]
+        };
+
+        $scope.selectedColor = 'black';
+        $scope.mudarCor = function(cor){
+            alert(cor);
+            $scope.selectedColor = cor;
+
+        };
           
-    }])
+    }]);
+})();
